b3bb5cc16bf0d9310c630576f4922d45645e936c
Maciej Chmiel <maciejowski2006yt@gmail.com>
2022-04-08 23:34:20 +0200

    Added more questions, fixed styles, and added refresh button

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -25,23 +25,53 @@ export default {
 					]
 				},
 				{
-					question: "Jaki stabilny networking jest aktualnie najbardziej zbliżony do HLAPI",
+					question: "Który z podanych networkingów jest najstabilniejszy i najbardziej zbliżony do HLAPI",
 					right: 1,    
 					answers: [
 						"MLAPI",
 						"Mirror",
-						"PUN(Phtoton Networking)",
+						"PUN2(Phtoton Networking 2)",
 						"Vivox"
 					]
 				},
 				{
 					question: "Jakiej platformy nie wspiera Unity 2019.X",
-					right: 1,    
+					right: 2,
 					answers: [
 						"PS5",
 						"XBOX Seriex",
 						"PowerPC",
 						"ARM"
+					]
+				},
+				{
+					question: "Jaka metoda nie może wykonać się wiele razy",
+					right: 3,
+					answers: [
+						"Update()",
+						"OnEnable()",
+						"OnCollisionEnter()",
+						"Start()"
+					]
+				},
+				{
+					question: "Jaki System Kontroli Wersji ma najlepszą integracje z Unity",
+					right: 0,
+					answers: [
+						"PlasticSCM",
+						"Git",
+						"Perforce",
+						"Unity Collaborate"
+					]
+				},
+				{
+					question: "Który z tych efektów audio nie jest dostępny w Unity",
+					right: 2,
+					answers: [
+						"Distortion",
+						"Reverb",
+						"Noise",
+						"Normalize"
 					]
 				}
 			],
@@ -51,13 +81,15 @@ export default {
 	},
 	methods: {
 		check: function(index, _index) {
-			if (this.answered[index] == true) {
+			if (this.answered[index]) {
 				return;
 			}
 			this.answered[index] = true;
 			
 			if (_index == this.elements[index].right) {
 				this.correctNumber++;
+				_index.classList.remove('disabled');
+				_index.classList.add('correct');
 			}
 		}
 	}
@@ -68,15 +100,15 @@ export default {
 	<nav>
 		<h2>Poprawne odpowiedzi: {{ correctNumber }}/{{elements.length}}</h2>
 	</nav>
-	<div class="separator"></div>
+	<div class="separator" id="top"></div>
 	<section v-for="(element, index) in elements" :key="element.index">
-		<h2>{{element.question}}</h2>
+		<h2>{{index + 1}}. {{element.question}}</h2>
 		<div class="buttons">
-			<button v-for="(answer, _index) in element.answers" :key="answer._index" @click="check(index, _index)">{{answer}}</button>
+			<button v-for="(answer, _index) in element.answers" :key="answer._index" :class="{default: !answered[index], correct: (_index === elements[index].right && answered[index]), disabled: (answered[index] && _index !== elements[index].right) }" @click="check(index, _index)">{{answer}}</button>
 		</div>
 	</section>
+	<button class="default reset" onclick="location.reload(); window.scrollTo(0, 0)">Jeszcze raz?</button>
 
-	
 </template>
 
 <style lang="scss">
@@ -92,7 +124,7 @@ nav {
 }
 
 section {
-	max-width: 35rem;
+	max-width: 40rem;
 	margin-inline: auto;
 	padding-bottom: 5rem;
 	display: flex;
@@ -109,31 +141,43 @@ section {
 	display: grid;
 	gap: 20px;
 	grid-template-columns: 1fr 1fr;
-	
-	button {
-		min-height: 5rem;
-		background-color: #3498db;
-		border: .25rem dashed #2980b9;
-		border-radius: 10px;
-		font-size: 1.25rem;
-
-		user-select: none;
-
-		cursor: pointer;
-		transition-duration: 200ms;
-		&:hover {
-			background-color: #e67e22;
-			border-color: #d35400;
-			border-width: 0.4rem;
-		}
-	}
 }
-.test {
-	color: red;
+button {
+	min-height: 5rem;
+	border-radius: 10px;
+	font-size: 1.25rem;
+	user-select: none;
+
+	transition-duration: 200ms;
+}
+.reset {
+	margin: 25px 45.8%;
+	min-width: 10rem;
+}
+.default {
+	background-color: #3498db;
+	border: .25rem dashed #2980b9;
+	cursor: pointer;
+
+	&:hover {
+		background-color: #e67e22;
+		border-color: #d35400;
+		border-width: 0.4rem;
+	}
 }
 .correct {
 	background-color: #2ecc71;
 	border: .25rem dashed #27ae60;
+	cursor: not-allowed;
+}
+.wrong {
+	background-color: #e74c3c;
+	border: .25rem dashed #c0392b;
+	cursor: not-allowed;
+}
+.disabled {
+	background-color: #95a5a6;
+	border: .25rem dashed #7f8c8d;
 	cursor: not-allowed;
 }
 </style>
